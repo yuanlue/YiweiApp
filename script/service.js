7d1aa7dd02cf773ece947eedc48dc3d78813d51f
yuanlue <381043186@qq.com>
2017-09-18 10:41:05 +0800

    test

---
+++ script/service.js
@@ -0,0 +1,7 @@
+
+  var now = Date.now();
+  var appKey = SHA1("A6059380178175"+"UZ"+"67146762-27C1-E14A-1645-EB18063350E9"+"UZ"+now)+"."+now
+  window.headers={
+    "X-APICloud-AppId": "A6059380178175",
+    "X-APICloud-AppKey": appKey
+  },
